4b7bb6693af36255663048a4a8a426ebc8520111
uttamgupta628 <98005363+uttamgupta628@users.noreply.github.com>
2024-08-29 17:26:36 +0530

    Add files via upload

---
+++ calculator/cal.js
@@ -0,0 +1,30 @@
+let string="";
+let buttons=document.querySelectorAll(".button");
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML=='='){
+            string=eval(string);
+            document.querySelector('input').value=string;
+        }
+        else if(e.target.innerHTML=='AC'){
+            string="";
+            document.querySelector('input').value=string;
+        }
+        else if (e.target.innerHTML === 'X') {
+            string += '*';
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML === 'DEC') {
+            string = string.slice(0, -1);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML === 'POW') {
+            string+='**';
+            document.querySelector('input').value = string;
+        }
+        else{
+            string=string+e.target.innerHTML;
+            document.querySelector('input').value=string;
+        }
+    })
+})
